3d85fe8d4cf9a5f029aa93e0ebe00c23bc9f7163
Mahsa Sinaei <mahsa.sinaei1991@gmail.com>
2025-07-08 17:36:39 -0400

    Adding main

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 
 from chatbot import ChatBot
 
+
 def main():
     load_dotenv()
     openai_api_key = os.environ.get("OPENAI_API_KEY")
-    
+
     chatbot = ChatBot(openai_api_key=openai_api_key)
     chatbot.run()
     
