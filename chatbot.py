21e5c40bf20ee081801f5aa5b00029314fc512c8
Mahsa Sinaei <mahsa.sinaei1991@gmail.com>
2025-07-08 17:19:27 -0400

    add environment example

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+
+class ChatBot:
+    def __init__(self, openai_api_key):
+        self.client = OpenAI(api_key=openai_api_key)
+    
+    def run(self):
+
+        history = []
+
+        while True:
+            user_input = input("User: ")
+            if user_input.lower() == "quit":
+                break
+
+            history.append({"role": "user", "content": user_input})
+
+            response = self.client.responses.create(model="gpt-4.1-nano", input=history)
+
+            print("ChatBot:", response.output_text)
+            history += [
+                {"role": "assistant", "content": response.output_text}
+            ]
---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+from chatbot import ChatBot
+
+def main():
+    load_dotenv()
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+    
+    chatbot = ChatBot(openai_api_key=openai_api_key)
+    chatbot.run()
+    
+
+if __name__ == "__main__":
+    main()
